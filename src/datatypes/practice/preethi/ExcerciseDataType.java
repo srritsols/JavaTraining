1dc62eb118b0b95ae10587f51c112e2a158ac955
Preethi <preethielavarasan94@gmail.com>
2019-11-01 10:35:11 -0400

    Update ExcerciseDataType.java

--- src/datatypes/practice/preethi/ExcerciseDataType.java
+++ src/datatypes/practice/preethi/ExcerciseDataType.java
@@ -15,6 +15,21 @@ public class ExcerciseDataType {
 	public static void main( String[] args ){  
  
 		//TODO you need to create your logic here..
+		int x=4;
+		int y=8;
+		double m=3.45;
+		double n=5.5;
+		boolean b1=true;
+		boolean b2=false;
+		System.out.println("b1&&b2 : " + (b1&&b2));
+		System.out.println("b1||b2 : " + (b1||b2));
+		System.out.println("!(b1&&b2) : " + !(b1&&b2));
+		System.out.println("x + y : " + (x+y));
+		System.out.println("x - y : " + (x-y));
+		System.out.println("n % m : "+ (n%m));
+		System.out.println("n / m : "+ (n/m));
+		
+		
 		
 
 	}
