ec3cd51a946108ca12d58f41d32072681a70b18f
Preethi <preethielavarasan94@gmail.com>
2019-11-04 14:06:39 -0500

    Update
    Updated Class and Control Statement exercise

--- src/classes/practice/preethi/ClassDemo.java
+++ src/classes/practice/preethi/ClassDemo.java
@@ -1,5 +1,5 @@
 package classes.practice.preethi;
-
+import java.util.Scanner;
 public class ClassDemo {
 
 	/**
@@ -8,15 +8,35 @@ public class ClassDemo {
 	 *  1. Create simple class with few variables
 	 *  2. Create few methods for class behavior 
 	 *  3. create a main method and object for newly created.
-	 *  4. Try to call differenet methods with multiple objects.
+	 *  4. Try to call different methods with multiple objects.
 	 *  
-	 */
-
+	 */ 
+	 public void fullThrottle()
+	 {
+	 System.out.println("The car is going as fast as it can"); 
+	 }
+	 public void speed(int maxSpeed) {
+		    System.out.println("Maximum speed is: " + maxSpeed);
+		  }
+	 public int convert(int maxSpeed) {
+		 int vel=0;
+		 vel= (maxSpeed*18)/5;
+		 return vel;
+	 }
+     
 	public static void main( String[] args ){  
  
 		//TODO you need to create your logic here..
-		
-		
+		ClassDemo myCar = new ClassDemo();            
+	    int vel=0,maxSpeed=0;
+	    Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the maximum speed");
+        maxSpeed=scanner.nextInt();
+        myCar.fullThrottle();     
+        myCar.speed(maxSpeed);
+	    vel=myCar.convert(maxSpeed);
+	    System.out.println("The speed in km/hr is " + vel);
+		scanner.close();
 
 	}
 }
--- src/controlstatements/practice/preethi/ControlStatement.java
+++ src/controlstatements/practice/preethi/ControlStatement.java
@@ -1,5 +1,6 @@
 package controlstatements.practice.preethi;
-
+import java.util.Random;
+import java.util.Scanner;
 public class ControlStatement {
 	
 	/**
@@ -16,6 +17,88 @@ public class ControlStatement {
 	public static void main(String[] args) { 
 		
 		//TODO here..
-		
-	} 
+		//For loop
+		int []arr=new int[5];
+		int sum=0;
+		Random rd=new Random();
+		for(int i=0;i<arr.length;i++)
+		arr[i]=rd.nextInt();
+		// Do while loop
+		int i=0;
+		do
+		{
+			sum+=arr[i];
+			i++;
+		}while(i<arr.length);
+		int i1=0;
+		// While loop
+		while(i1<arr.length)
+		{
+			System.out.println(arr[i1]);
+			i1++;
+		}
+        int max=0;
+        for(int j=arr.length-1;j>=0;j--)
+        {
+        if(arr[j]>max)
+          max=arr[j];
+        }
+        System.out.println("The highest number is" + max);
+        System.out.println("The Average number is" + (sum/(arr.length)));
+        for(int i2=0;i2<arr.length;i2++)
+        {
+        	if(arr[i2]>(sum/arr.length))
+        	System.out.println(arr[i2]+ " is greater than" + (sum/arr.length));
+        	else
+        	continue;
+        } 
+        // Switch Case
+        double d1,d2,result;
+        int flag=0;
+        d1=0;d2=0;result=0;
+        char operator;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the first number");
+        d1=scanner.nextDouble();
+        System.out.println("Enter the second number");
+        d2=scanner.nextDouble();
+        System.out.print("Enter an operator");
+        operator=scanner.next().charAt(0);
+        switch(operator)
+        {
+        case'+':
+        	result=d1+d2;
+        	break;
+        case'-':
+        	result=d1-d2;
+        	break;
+        case'*':
+        	result=d1*d2;
+            break;
+        case'/':
+        	result=d1/d2;
+        	break;
+        case'%':
+        	result=d1%d2;
+        	break;
+        default: 
+        	System.out.println("Invalid operator");
+        	flag=1;
+        }
+        //If else
+        if((flag==0))
+        	System.out.println("The result is "+ result);
+        else
+        	System.out.println("Enter a valid operator");
+        int a,b;
+        System.out.println("Enter the first number");
+        a=scanner.nextInt();
+        System.out.println("Enter the second number");
+        b=scanner.nextInt(); //Logical Operations
+        System.out.println("a | b =" + (a|b ));
+        System.out.println("a & b =" + (a&b ));
+        System.out.println("a ^ b =" + (a^b ));
+        scanner.close();
+        
+	}  
 }
