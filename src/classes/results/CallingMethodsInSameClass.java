752375940eb64aacfc80af955ac24c6e8da6d01a
Arun <srini@srritsolutions.com>
2019-11-05 21:40:12 -0500

    Added results programs

---
+++ src/classes/results/CallingMethodsInSameClass.java
@@ -0,0 +1,19 @@
+package classes.results;
+
+public class CallingMethodsInSameClass
+{
+	public static void main(String[] args) {
+		printOne();
+		printOne();
+		printTwo();
+	}
+
+	public static void printOne() {
+		System.out.println("Hello World");
+	}
+
+	public static void printTwo() {
+		printOne();
+		printOne();
+	}
+}
---
+++ src/inheritance/practice/AnimalInheritanceTest.java
@@ -0,0 +1,14 @@
+package inheritance.practice;
+
+public class AnimalInheritanceTest {
+
+	public static void main(String[] args) {
+		Cat cat = new Cat(false, "milk", 4, "black");
+
+		System.out.println("Cat is Vegetarian?" + cat.isVegetarian());
+		System.out.println("Cat eats " + cat.getEats());
+		System.out.println("Cat has " + cat.getNoOfLegs() + " legs.");
+		System.out.println("Cat color is " + cat.getColor());
+	}
+
+}
