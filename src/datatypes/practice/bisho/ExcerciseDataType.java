61ba7964bd5deef56fa2e4301509340ec795fc63
bishosansaryal <bishosansaryal@gmail.com>
2019-11-07 15:01:17 -0500

    datatype

--- src/datatypes/practice/bisho/ExcerciseDataType.java
+++ src/datatypes/practice/bisho/ExcerciseDataType.java
@@ -14,9 +14,45 @@ public class ExcerciseDataType {
 	public static void main( String[] args ){  
  
 		//TODO you need to create your logic here..
-		int a;
+		// items bought
+		int bisho= 30;						// int datatype
+		int nitesh= 32;						// int datatype
+		int average= (bisho+nitesh)/2;  	//average
+		int difference= (nitesh-bisho);  	// difference
+		int sum= (bisho+nitesh);			// sum
 		
-
+		// sum spent
+		double bishospent= 30.00;			//double datatype
+		double niteshspent= 32.00;			//double datatype
+		
+		//cost of each product
+		int bishocost= (int) (bishospent/bisho);	//Arithmetic operators
+		int niteshcost= (int) (niteshspent/nitesh);	//Arithmetic operators
+		
+		if (bishocost>niteshcost)					//if-else if-else statement
+		{
+			System.out.println(bishocost);
+		}
+		else if (niteshcost>bishocost)
+		{
+			System.out.println(niteshcost);
+		}
+		else 
+		{
+			System.out.println("Bought more items" +" "+difference);
+		}
+		
+		// and- if operator 
+		System.out.println((nitesh<bisho) && (difference<=2));
+		System.out.println((nitesh<bisho) || (difference<=2));
+		System.out.println((nitesh>bisho) && (difference<=2));
+		System.out.println((nitesh>bisho) || (difference<=2));
+		System.out.println((nitesh>bisho) && (difference>=2));
+		
+		if (nitesh>bisho)
+			System.out.println("items bought"+ " "+nitesh);
+		else if (nitesh<bisho)
+			System.out.println("items bought"+ " "+bisho);
 	}
 }
 
