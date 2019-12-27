11e83459e3a49a8de1cde02600a2199574f67923
Tanveerx81 <tanveer.x81@gmail.com>
2019-12-26 19:33:01 -0500

    Revert "Datatypes, Classes, ControlStatement, Inheritance."
    This reverts commit 9bf6bf2157bf76540cc0bccd5e1d9e63fd3418d8.

--- core_java/src/classes/practice/tanveer/InterfaceDemo.java
+++
@@ -1,6 +0,0 @@
-package com.classes;
-
-public interface InterfaceDemo {
-    int add(int a, int b);
-    void display();
-}
--- core_java/src/datatypes/practice/tanveer/TestDataMultiply.java
+++
@@ -1,8 +0,0 @@
-package com.datatypes;
-
-public class TestDataMultiply {
-    public static void main(String[] args) {
-        int n1 = 25, n2 = 5;
-        System.out.println("Product: "+ n1*n2);
-    }
-}
